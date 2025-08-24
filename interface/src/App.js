76c7dc42d540fc8efaabd2b194a52a99e6e5a7e5
Savindu Sandumina <savindussb@gmail.com>
2025-08-24 18:26:56 +1000

    Update App.js

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
 import Dashboard from './components/Dashboard';
 import DataCollection from './components/DataCollection';
 import ComplianceLogs from './components/ComplianceLogs';
@@ -8,12 +9,21 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/data-collection" element={<DataCollection />} />
-        <Route path="/compliance-logs" element={<ComplianceLogs />} />
+        <Route path="/" element={<Layout />}>
+          {/* Default route */}
+          <Route index element={<Dashboard />} />
+
+          {/* Page routes */}
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="data-collection" element={<DataCollection />} />
+          <Route path="compliance-logs" element={<ComplianceLogs />} />
+        </Route>
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
 }
 
-
+export default App;
